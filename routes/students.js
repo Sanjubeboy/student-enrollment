b94346269c2b3bd472ac2c32ba042fd518aed3fb
Sanjubeboy <sanjubeboy@gmail.com>
2023-06-04 23:27:46 +0530

    Updated MVC architecture

--- routes/students.js
+++ routes/students.js
@@ -3,22 +3,10 @@ const router = express.Router()
 const studentModel = require('../models/students')
 const {getAllStudents, addStudents, getStudentById, updatedStudent, deleteStudent, getStudent} = require('../controllers/students')
 
-//getAllStudents
-// router.get('/', getAllStudents)
 
-//addStudents
-// router.post('/', addStudents)
 
 router.route('/').get(getAllStudents).post(addStudents)
 router.route('/:id').get(getStudent, getStudentById).patch(getStudent, updatedStudent).delete(getStudent, deleteStudent)
 
-//getStudentById
-// router.get('/:id', getStudent, getStudentById)
-
-//updateStudent
-// router.patch('/:id', getStudent,updatedStudent)
-
-//deleteStudent
-// router.delete('/:id', getStudent, deleteStudent)
 
 module.exports = router

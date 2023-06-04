7522893233adab6e216aba4893e51cbdda980893
Sanjubeboy <sanjubeboy@gmail.com>
2023-06-04 23:27:14 +0530

    Updated MVC architecture

---
+++ controllers/students.js
@@ -0,0 +1,94 @@
+const studentModel = require('../models/students')
+
+const getAllStudents = async(req, res) => {
+    try{
+        const students = await studentModel.find();
+        res.status(200).json(students)
+    }
+    catch(error){
+        res.status(500).json({message:error.message})
+    }
+}
+
+const addStudents = async(req, res) => {
+    const newStudent = new studentModel({
+        name: req.body.name,
+        enrolledDepartment: req.body.enrolledDepartment,
+        enrollmentDate: req.body.enrollmentDate
+    })
+
+    try{
+        const students = await newStudent.save()
+        res.status(201).json(students)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+
+const getStudentById = (req, res) => {
+    
+    try{
+        res.status(200).json(res.student)
+    }
+    catch(error){
+        res.send(error)
+    }
+}
+
+
+const updatedStudent = async (req, res) => {
+    if(req.body.name != null)
+    {
+        res.student.name = req.body.name
+    }
+    if(req.body.enrolledDepartment != null)
+    {
+        res.student.enrolledDepartment = req.body.enrolledDepartment
+    }
+    if(req.body.enrollmentDate != null)
+    {
+        res.student.enrollmentDate = req.body.enrollmentDate
+    }
+
+    try{
+        const updatedStudent = await res.student.save()
+        res.status(200).json(updatedStudent)
+    }
+
+    catch(error){
+        res.status(400).json({message: error.message})
+    }
+}
+
+const deleteStudent = async(req, res) => {
+    try{
+        await res.student.deleteOne()
+        res.status(200).json({message: `Deleted the user ${res.student.name}`})
+    }
+
+    catch(error){
+        res.status(400).json({message: error.message})
+    }
+}
+
+
+const getStudent = async (req, res, next) => {
+    let student
+    try{
+        student = await studentModel.findById(req.params.id)
+        if(student == null){
+            return res.status(404).json({message:`Cannot find user id ${req.params.id}`})
+        }
+    }
+    catch(error) {
+        return res.status(500).json({message: error.message})
+    }
+
+    res.student = student;
+    next()
+
+}
+
+module.exports = {getAllStudents, addStudents, getStudentById, updatedStudent, deleteStudent, getStudent}
--- routes/students.js
+++ routes/students.js
@@ -1,107 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const studentModel = require('../models/students')
+const {getAllStudents, addStudents, getStudentById, updatedStudent, deleteStudent, getStudent} = require('../controllers/students')
 
 //getAllStudents
-router.get('/', async(req, res) => {
-    try{
-        const students = await studentModel.find();
-        res.status(200).json(students)
-    }
-    catch(error){
-        res.status(500).json({message:error.message})
-    }
-})
+// router.get('/', getAllStudents)
 
 //addStudents
-router.post('/', async(req, res) => {
-    const newStudent = new studentModel({
-        name: req.body.name,
-        enrolledDepartment: req.body.enrolledDepartment,
-        enrollmentDate: req.body.enrollmentDate
-    })
+// router.post('/', addStudents)
 
-    try{
-        const students = await newStudent.save()
-        res.status(201).json(students)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+router.route('/').get(getAllStudents).post(addStudents)
+router.route('/:id').get(getStudent, getStudentById).patch(getStudent, updatedStudent).delete(getStudent, deleteStudent)
 
 //getStudentById
-router.get('/:id', getStudent, (req, res) => {
-    // res.send(`Displaying student with id ${req.params.id}`)
-    // try{
-    //     const student = await studentModel.find({_id:req.params.id})
-    //     res.status(200).json(student)
-    // }
-    // catch(error){
-    //     res.status(500).json({message: error.message})
-    // }
-    try{
-        res.status(200).json(res.student)
-    }
-    catch(error){
-        res.send(error)
-    }
-})
+// router.get('/:id', getStudent, getStudentById)
 
 //updateStudent
-router.patch('/:id', getStudent,async (req, res) => {
-    if(req.body.name != null)
-    {
-        res.student.name = req.body.name
-    }
-    if(req.body.enrolledDepartment != null)
-    {
-        res.student.enrolledDepartment = req.body.enrolledDepartment
-    }
-    if(req.body.enrollmentDate != null)
-    {
-        res.student.enrollmentDate = req.body.enrollmentDate
-    }
-
-    try{
-        const updatedStudent = await res.student.save()
-        res.status(200).json(updatedStudent)
-    }
-
-    catch(error){
-        res.status(400).json({message: error.message})
-    }
-})
+// router.patch('/:id', getStudent,updatedStudent)
 
 //deleteStudent
-router.delete('/:id', getStudent, async(req, res) => {
-    try{
-        await res.student.deleteOne()
-        res.status(200).json({message: `Deleted the user ${res.student.name}`})
-    }
-
-    catch(error){
-        res.status(400).json({message: error.message})
-    }
-})
-
-
-async function getStudent(req, res, next) {
-    let student
-    try{
-        student = await studentModel.findById(req.params.id)
-        if(student == null){
-            return res.status(404).json({message:`Cannot find user id ${req.params.id}`})
-        }
-    }
-    catch(error) {
-        return res.status(500).json({message: error.message})
-    }
-
-    res.student = student;
-    next()
-
-}
-
+// router.delete('/:id', getStudent, deleteStudent)
 
 module.exports = router
